8294d5b99d22dbf019b421dfda4bef88593438d5
moalnatour <m.alnatour@repzoapp.com>
2025-08-28 12:55:19 +0300

    Refactor type definitions in AggregationBuilder for improved clarity and consistency

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -486,9 +486,9 @@ export default class AggregationBuilder {
     dateFromString: (dateString: String | any, format?: String | any, timezone?: string | any, onNull?: string | any, options?: Options) => {
         $dateFromString: {
             dateString: string | any;
-            format?: string | undefined;
-            timezone?: string | undefined;
-            onNull?: string | undefined;
+            format?: string;
+            timezone?: string;
+            onNull?: string;
         };
     };
     /**
@@ -1030,7 +1030,7 @@ export default class AggregationBuilder {
     switch: (branches: any[], arg?: string | any) => {
         $switch: {
             branches: any[];
-            default?: string | undefined;
+            default?: string;
         };
     };
     /**
@@ -1044,7 +1044,7 @@ export default class AggregationBuilder {
     map: (input: string | any, as?: string, expr?: any) => {
         $map: {
             input: string;
-            as?: string | undefined;
+            as?: string;
             in: any;
         };
     };
@@ -1238,8 +1238,8 @@ export default class AggregationBuilder {
     searchAutocomplete: ({ path, query, tokenOrder, fuzzy, }: {
         path: string | string[];
         query: string | string[];
-        tokenOrder?: "any" | "sequential" | undefined;
-        fuzzy?: FuzzyOptions | undefined;
+        tokenOrder?: "sequential" | "any";
+        fuzzy?: FuzzyOptions;
     }) => AutocompleteOperator;
     searchShould: (operator: SearchCompoundOperator, minimumShouldMatch?: number) => AggregationBuilder;
     searchMust: (operator: SearchCompoundOperator) => AggregationBuilder;
